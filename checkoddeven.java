5a6af09d47f81782ae4446c6222fac536e0cf263
Swathi16717 <elavarsiswathi@gmail.com>
2024-08-11 17:33:38 +0530

    Add files via upload

---
+++ checkoddeven.java
@@ -0,0 +1,15 @@
+package myfirstjava;
+
+public class checkoddeven {
+	public static void main(String[] args) {
+		int n=17;
+		if(n % 2 ==0) {
+			System.out.println("THE GIVEN NUMBER IS EVEN");
+			
+		}else {
+			System.out.println("THE GIVEN NUMBER IS ODD");
+		}
+			
+	}
+
+}
