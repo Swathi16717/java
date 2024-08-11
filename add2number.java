5dab83ed285d942cf97114d136badbf912f30d4e
Swathi16717 <elavarsiswathi@gmail.com>
2024-08-11 17:29:13 +0530

    Add files via upload

---
+++ add2number.java
@@ -0,0 +1,12 @@
+package myfirstjava;
+
+public class add2number {
+	static int x=10;
+	static int y=20;
+	static int z;
+	public static void main(String[]args) {
+		z= x+y;
+	System.out.println("sum is "+ z);
+
+}
+}
