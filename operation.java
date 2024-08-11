08a4a9905a4c4662440536d8d64d189e0d246d81
Swathi16717 <elavarsiswathi@gmail.com>
2024-08-11 17:34:30 +0530

    Add files via upload

---
+++ operation.java
@@ -0,0 +1,17 @@
+package myfirstjava;
+
+public class operation {
+	public static void main(String[] args) {
+		int a=20;
+		int b=30;
+		int sum=a+b;
+		System.out.println("Addition="+sum);
+		int sub=a-b;
+		System.out.println("Subtraction="+sub);
+		int multi=a*b;
+		System.out.println("Multiplication="+multi);
+		
+		
+	}
+
+}
