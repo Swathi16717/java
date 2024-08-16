3aaf1a6233fd56a57cd89cbc312a5470cf44515b
Swathi16717 <elavarsiswathi@gmail.com>
2024-08-16 10:29:00 +0530

    Add files via upload

---
+++ WrapperUnboxing.java
@@ -0,0 +1,12 @@
+package myfirstjava;
+public class WrapperUnboxing {
+public static void main(String[] args) {
+	Integer a = 40;
+	int b = a;
+	System.out.println("primitive int:" +b);
+	Boolean c = true;
+	boolean d = c;
+	System.out.println("primitive Boolean:" +d);
+}
+}
+
